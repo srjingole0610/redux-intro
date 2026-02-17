c8dbe56b64c638fcd9fce022f12da7e6605932a9
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-17 22:05:23 +0530

    Enhance BalanceDisplay to use Redux state for balance and add currency conversion logic in accountSlice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -40,9 +40,39 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = "";
     },
+
+    convertingCurrency(state, action) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
-console.log(requestLoan(1000, "Buy a car"));
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") {
+    return {
+      type: "account/deposit",
+      payload: amount,
+      isLoading: false,
+    };
+  }
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    //API CALL
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&symbols=USD`,
+    );
+    const data = await res.json();
+    console.log(data);
+    const converted = data.rates.USD;
+
+    //Return the action
+    dispatch({
+      type: "account/deposit",
+      payload: converted,
+    });
+  };
+}
+
 export default accountSlice.reducer;
