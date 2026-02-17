35abc1e47a12c29fb7b8b2baf4d98e37559c59d9
Suraj Ingole <suraj.ingole0610@gmail.com>
2026-02-17 16:32:22 +0530

    Implement customer creation and account operations with Redux integration

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import "./index.css";
 import App from "./App";
 import store from "./store";
 
-store.dispatch({
-  type: "account/deposit",
-  payload: 500,
-});
-console.log(store.getState());
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
